442981856e7db41e9186d3712e8c27d8851fb0f9
e1b19025 <e1b19025@oit.ac.jp>
2021-10-02 01:25:54 +0900

    Delete NavMeshComponent 2D and  contents of the "chasePlayer" method
    Because I decided that it could not be implemented

--- test_1/Assets/scripts/CharacterFollder/Enemy.cs
+++ test_1/Assets/scripts/CharacterFollder/Enemy.cs
@@ -13,8 +13,6 @@ public class Enemy : Character
     Slider HPvar; /*‘Ì—Íƒo[*/
     Text NameText;
 
-    private NavMeshAgent agent;
-
 
     public override void attack()
     {
@@ -31,23 +29,12 @@ public class Enemy : Character
 
     private void moveToPlayer()
     {
-        GameObject target = GameObject.FindGameObjectWithTag("Player");
-
-        if(target)
-        {
-            agent.destination = target.transform.position;
-        }
-
 
     }
 
     void Start()
     {
         hp = enemyStatus.getMaxHP();/*hp‚ÉmaxHP‚ğ‘ã“ü*/
-        agent = GetComponent<NavMeshAgent>();
-
-        
-
         HPvar = transform.Find("Canvas/HPBar").gameObject.GetComponent<Slider>();
         NameText = transform.Find("Canvas/Name").gameObject.GetComponent<Text>();
     }
