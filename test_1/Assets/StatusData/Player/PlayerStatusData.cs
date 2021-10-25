98a9b817a65eda24dd5b20e6f73541d52800ffe0
Kissy <72001573+kissy1221@users.noreply.github.com>
2021-10-25 13:20:39 +0900

    古いUnityプロジェクトを削除

--- test_1/Assets/StatusData/Player/PlayerStatusData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu]
-public class PlayerStatusData : CharacterStatus
-{
-    bool invincible = false; //–³“G
-}
