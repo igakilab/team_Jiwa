e14211cd260f09fda2eb7b4d35f2e9d44dd6a104
medo <ryugom.0523@gmail.com>
2021-10-29 01:08:44 +0900

    ステージセレクトのUI周りを変更
    ステージセレクト画面のUIを調整

--- RoguLikeActionRPG/Assets/scripts/stageSelector.cs
+++ RoguLikeActionRPG/Assets/scripts/stageSelector.cs
@@ -3,19 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class stageSelector : MonoBehaviour
 {
     // Start is called before the first frame update
     public Button FirstSelectButton;
+    public GameObject btn;
+
+    private Button prev_selected;
+
     void Start()
     {
         FirstSelectButton.Select();
+        btn = EventSystem.current.currentSelectedGameObject;
     }
 
     public void OnClick(int number)
     {
         Debug.Log("go to stage " + number + "!");
         SceneManager.LoadScene("Stage" + number);
+    }
+
+    public void Update()
+    {
+        btn = EventSystem.current.currentSelectedGameObject;
+        if(btn == null)
+        {
+            prev_selected.Select();
+        }
+        else
+        {
+            prev_selected = btn.GetComponent<Button>();
+        }
     }
 }
